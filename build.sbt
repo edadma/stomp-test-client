6d24923effe26bde283c963a28415bb8ac1cbc41
Edward A Maxedon, Sr <edadma@vinctus.com>
2019-10-16 15:18:16 -0400

    first commit

---
+++ build.sbt
@@ -0,0 +1,70 @@
+name := "stomp-test-client"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+scalacOptions ++= Seq( "-deprecation", "-feature", "-unchecked", "-language:postfixOps", "-language:implicitConversions", "-language:existentials",
+  "-P:scalajs:sjsDefinedByDefault" )
+
+organization := "xyz.hyperreal"
+
+Global / onChangedBuildSource := ReloadOnSourceChanges
+
+resolvers += "Typesafe Repository" at "https://repo.typesafe.com/typesafe/releases/"
+
+resolvers += "Hyperreal Repository" at "https://dl.bintray.com/edadma/maven"
+
+enablePlugins(ScalaJSPlugin)
+
+enablePlugins(ScalaJSBundlerPlugin)
+
+scalaJSUseMainModuleInitializer := true
+
+jsEnv := new org.scalajs.jsenv.nodejs.NodeJSEnv()
+
+npmDependencies in Compile ++= Seq(
+  "sockjs-client" -> "1.4.0",
+  "stompjs" -> "2.3.3"
+)
+
+libraryDependencies ++= Seq(
+  ScalablyTyped.S.`sockjs-client`,
+  ScalablyTyped.S.stompjs
+)
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %%% "scalatest" % "3.0.8" % "test",
+  "org.scalacheck" %%% "scalacheck" % "1.14.1" % "test"
+)
+
+coverageExcludedPackages := ".*Main"
+
+mainClass in (Compile, run) := Some( "xyz.hyperreal." + name.value.replace('-', '_') + ".Main" )
+
+mainClass in assembly := Some( "xyz.hyperreal." + name.value.replace('-', '_') + ".Main" )
+
+assemblyJarName in assembly := name.value + "-" + version.value + ".jar"
+
+publishMavenStyle := true
+
+publishArtifact in Test := false
+
+pomIncludeRepository := { _ => false }
+
+licenses := Seq("ISC" -> url("https://opensource.org/licenses/ISC"))
+
+homepage := Some(url("https://github.com/edadma/" + name.value))
+
+pomExtra :=
+  <scm>
+    <url>git@github.com:edadma/{name.value}.git</url>
+    <connection>scm:git:git@github.com:edadma/{name.value}.git</connection>
+  </scm>
+  <developers>
+    <developer>
+      <id>edadma</id>
+      <name>Edward A. Maxedon, Sr.</name>
+      <url>https://github.com/edadma</url>
+    </developer>
+  </developers>
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,66 @@
+package xyz.hyperreal.stomp_test_client
+
+import typings.node.{nodeStrings, process}
+import typings.sockjsDashClient.sockjsDashClientMod
+import typings.stompjs.stompjsMod
+import typings.stompjs.stompjsMod.{Client, Frame, Message}
+
+import scala.scalajs.js
+
+
+object Main extends App {
+
+  val serverHostname = "192.168.0.117"
+  val serverPort = 15674
+  val serverPath = "/stomp"
+
+  val client = stompjsMod.over( new sockjsDashClientMod.^(s"http://$serverHostname:$serverPort$serverPath") )
+
+  val sendRegex = """([a-z0-9-]*):\s*(.*)"""r
+  val subscribeRegex = """\+(.+)"""r
+  val unsubscribeRegex = """\-(.+)"""r
+
+  var current: String = null
+
+  client.connect(js.Dynamic.literal(), frame =>
+    frame.asInstanceOf[js.Dictionary[String]]("command") match {
+      case "CONNECTED" =>
+        println( s"connected to ${frame.asInstanceOf[js.Dictionary[Any]]("headers").asInstanceOf[js.Dictionary[String]]("server")}\n" )
+        println(
+          """
+            |type a command
+            |  <queue>:<message>    if <queue> is empty, the previous queue that was messaged will be used as destination
+            |  +<queue>             subscribe to <queue>
+            |  -<queue>             unsubscribe from <queue>
+            |""".stripMargin
+        )
+      case _ => println( js.JSON.stringify(frame) )
+    } )
+
+  process.stdin.setEncoding( "utf-8" )
+
+  process.stdin.on_data( nodeStrings.data, line =>
+    line.asInstanceOf[String].trim match {
+      case sendRegex( queue, message ) =>
+        val dest = if (queue isEmpty) current else queue
+
+        if (dest eq null)
+          println( "specify queue" )
+        else {
+          client.send( dest, js.Dynamic.literal(), message )
+          current = dest
+        }
+      case subscribeRegex( queue ) =>
+        println( s"subscribing to '$queue'" )
+        current = queue
+        client.subscribe( queue, (message: Message) =>
+          message.command match {
+            case "MESSAGE" =>
+              println( s"($queue) ${message.body}" )
+            case _ =>
+              println( message )
+          } )
+      case command => println( s"unrecognized command '$command'" )
+    } )
+
+}
